661c2041ced02640b5c89b0cca6ee74b862cf9ee
kevin leichner <kevinmitrix@gmail.com>
2025-10-07 21:00:42 -0300

    Agrego OPTIONS a cors

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ dbConnection();
 app.use(cors({
     origin: process.env.URL_FRONTEND,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-token']
+}));
+
+app.options('*', cors({
+    origin: process.env.URL_FRONTEND,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-token']
 }));
 
